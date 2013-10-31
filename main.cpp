49d71f1df9b8687c593d7341bdf6910cad92e6ca
Marijan Svalina <marijan.svalina@gmail.com>
2013-10-31 11:53:52 +0100

    modifed inputs for my pointcloud

--- main.cpp
+++ main.cpp
@@ -11,16 +11,16 @@ int main(int argc, char *argv[])
     //QCoreApplication a(argc, argv);
 
     // Load input file into a PointCloud<T> with an appropriate type
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::PCLPointCloud2 cloud_blob;
-    pcl::io::loadPCDFile ("bun0.pcd", cloud_blob);
+    pcl::io::loadPCDFile ("pointcloud-ascii-downsampled-inliers.pcd", cloud_blob);
     pcl::fromPCLPointCloud2 (cloud_blob, *cloud);
     //* the data should be available in cloud
 
     // Normal estimation*
-    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+    pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> n;
     pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
     tree->setInputCloud (cloud);
     n.setInputCloud (cloud);
     n.setSearchMethod (tree);
@@ -29,28 +29,28 @@ int main(int argc, char *argv[])
     //* normals should not contain the point normals + surface curvatures
 
     // Concatenate the XYZ and normal fields*
-    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
     pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
     //* cloud_with_normals = cloud + normals
 
     // Create search tree*
-    pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
+    pcl::search::KdTree<pcl::PointXYZRGBNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointXYZRGBNormal>);
     tree2->setInputCloud (cloud_with_normals);
 
     // Initialize objects
-    pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+    pcl::GreedyProjectionTriangulation<pcl::PointXYZRGBNormal> gp3;
     pcl::PolygonMesh triangles;
 
     // Set the maximum distance between connected points (maximum edge length)
-    gp3.setSearchRadius (0.025);
+    gp3.setSearchRadius (0.125);
 
     // Set typical values for the parameters
     gp3.setMu (2.5);
-    gp3.setMaximumNearestNeighbors (100);
+    gp3.setMaximumNearestNeighbors (50);
     gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
     gp3.setMinimumAngle(M_PI/18); // 10 degrees
     gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
-    gp3.setNormalConsistency(false);
+    gp3.setNormalConsistency(true);
 
     // Get result
     gp3.setInputCloud (cloud_with_normals);
