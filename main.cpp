388c81e274608a30055d3fd692117193a06e7922
Marijan Svalina <marijan.svalina@gmail.com>
2013-11-06 10:13:16 +0100

    Changed from organized fast mesh to poisson

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/surface/gp3.h>
-#include <pcl/surface/impl/organized_fast_mesh.hpp>
+#include <pcl/surface/poisson.h>
 #include <pcl/io/vtk_io.h>
 
-typedef pcl::PointXYZ PointType;
+//typedef pcl::PointXYZ PointType;
 typedef pcl::Normal Normal;
-typedef pcl::PointNormal PointTypeN;
-//typedef pcl::PointXYZRGB PointType;
-//typedef pcl::PointXYZRGBNormal PointTypeN;
+//typedef pcl::PointNormal PointTypeN;
+typedef pcl::PointXYZRGB PointType;
+typedef pcl::PointXYZRGBNormal PointTypeN;
 
 int main(int argc, char *argv[])
 {
@@ -18,22 +18,22 @@ int main(int argc, char *argv[])
     // Load input file into a PointCloud<T> with an appropriate type
     pcl::PointCloud<PointType>::Ptr cloud (new pcl::PointCloud<PointType>);
     pcl::PCLPointCloud2 cloud_blob;
-    //pcl::io::loadPCDFile ("pointcloud-ascii-downsampled-inliers.pcd", cloud_blob);
-    pcl::io::loadPCDFile ("bun0.pcd", cloud_blob);
+    pcl::io::loadPCDFile ("pointcloud-ascii-downsampled-inliers.pcd", cloud_blob);
+    //pcl::io::loadPCDFile ("bun0.pcd", cloud_blob);
     pcl::fromPCLPointCloud2 (cloud_blob, *cloud);
     //* the data should be available in cloud
-    std::cout << "bun0 loaded " << std::endl;
+    std::cout << "cloud loaded " << std::endl;
     std::cout << cloud->size() << std::endl;
 
     // Normal estimation*
-    pcl::NormalEstimation<PointType, Normal> n;
+    pcl::NormalEstimation<PointType, Normal> normEst;
     pcl::PointCloud<Normal>::Ptr normals (new pcl::PointCloud<Normal>);
     pcl::search::KdTree<PointType>::Ptr tree (new pcl::search::KdTree<PointType>);
     tree->setInputCloud (cloud);
-    n.setInputCloud (cloud);
-    n.setSearchMethod (tree);
-    n.setKSearch (20);
-    n.compute (*normals);
+    normEst.setInputCloud (cloud);
+    normEst.setSearchMethod (tree);
+    normEst.setKSearch (20);
+    normEst.compute (*normals);
     //* normals should not contain the point normals + surface curvatures
 
     // Concatenate the XYZ and normal fields*
@@ -46,16 +46,16 @@ int main(int argc, char *argv[])
     tree2->setInputCloud (cloud_with_normals);
 
     // Initialize objects
-    // pcl::GreedyProjectionTriangulation<pcl::PointXYZRGBNormal> gp3;
-    pcl::OrganizedFastMesh<PointTypeN> ofm;
+    pcl::Poisson<PointTypeN> poissn;
     pcl::PolygonMesh triangles;
 
     std::cout << cloud_with_normals->size() << std::endl;
 
-    ofm.setSearchMethod(tree2);
-    ofm.setInputCloud(cloud_with_normals);
-    ofm.reconstruct (triangles);
+    poissn.setInputCloud(cloud_with_normals);
+    poissn.setSearchMethod(tree2);
+    poissn.reconstruct (triangles);
+    poissn.setOutputPolygons(false);
 
-    pcl::io::saveVTKFile ("mesh-ofm.vtk", triangles);
+    pcl::io::saveVTKFile ("mesh-poissn.vtk", triangles);
     return 0;
 }
